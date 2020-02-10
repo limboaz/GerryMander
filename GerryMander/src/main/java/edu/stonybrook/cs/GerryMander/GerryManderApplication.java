0dc6935ae44c93d17d55f00dafc1043d19a51a7f
limboaz <limboaz@outlook.com>
2020-02-10 17:03:11 -0500

    Project initialized

---
+++ GerryMander/src/main/java/edu/stonybrook/cs/GerryMander/GerryManderApplication.java
@@ -0,0 +1,13 @@
+package edu.stonybrook.cs.GerryMander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerryManderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerryManderApplication.class, args);
+	}
+
+}
