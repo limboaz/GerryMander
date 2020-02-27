442a0511cf5d2149201321f065e501115d133a87
Melanie Logan <melanie.logan@stonybrook.edu>
2020-02-26 23:52:20 -0500

    using csv file for the unique indices per county, less human-error prone

--- ElectionResults/src/AZ_electionware_parse.py
+++ ElectionResults/src/AZ_electionware_parse.py
@@ -1,6 +1,12 @@
 import pandas as pd
 #import os
 
+#extract boundaries for values in ElectionWare file
+def county_key_value(county_key, col):
+    row = county_key[county_key.FIELD_TYPE == col].index[0]
+    indices = [county_key.at[row, "START_POSITION"]-1, county_key.at[row, "END_POSITION"]]
+    return indices
+
 #list of counties that use ElectionWare
 counties_ew_16 = ["Apache", "Cochise", "Gila", "Graham", "Greenlee", "Mohave", "Navajo", "Pima", "Pinal", "Santa_Cruz"]
 counties_ew_18 = ["Apache", "Cochise", "Gila", "Graham", "Greenlee", "Mohave", "Navajo", "Pima", "Pinal", "Santa_Cruz", "La_Paz", "Yuma"]
@@ -12,52 +18,24 @@ rows = []
 #iterate through each election year
 index = 0
 for year in years:
+    #load key for cleaning data
+    file = "../raw_data/Arizona/EW_"+str(year[0:4])+".csv"
+    key_EW = pd.read_csv(file)
     #iterate through each county
     for county in counties_ew[0]:
+        #filter key to just the current county
+        county_key = key_EW[key_EW.COUNTY == county]
+        #open election results for that county
         file = open('../raw_data/Arizona/'+str(county)+'/'+str(year)+'_results_county.txt', 'r')
+        #create election result dataframe
         df_AZ = pd.DataFrame(columns = ["Year","CountyName","ContestID", "ChoiceID", "PrecinctID", "VoteTotal", "PollingPlaceVotes", "EarlyVotes", "ProvisionalVotes", "OtherVotes",
         "PartyName", "ContestName", "ChoiceName", "PrecinctDesignation", "PrecinctName", "SubJurisidiction", "VotesAllowed", "ReferendumFlag"])
-        polling_places_indices = [17,23]
-        early_votes_indices = [23,29]
-        provisional_indices = [29,35]
-        precinct_name_indices = [205,235]
+        #set indices accordingly
+        polling_places_indices = county_key_value(county_key, "Polling Place Votes")
+        early_votes_indices = county_key_value(county_key, "Early Votes")
+        provisional_indices = county_key_value(county_key, "Provisional Votes")
+        precinct_name_indices = county_key_value(county_key, "Precinct Name")
         subJur_start_index = 234
-        #alter for Apache
-        if county == "Apache":
-            if year == "2018_con":
-                early_votes_indices = [17,23]
-                polling_places_indices = [23,29]
-            precinct_name_indices = [207,234]
-        #alter for Cochise
-        elif county == "Cochise":
-            precinct_name_indices = [207,235]
-            subJur_start_index = 235
-            early_votes_indices = [17,23]
-            polling_places_indices = [23,29]
-        #alter for Mohave
-        elif county == "Mohave" and year == "2018_con":
-            early_votes_indices = [17,23]
-            polling_places_indices = [23,29]
-        #alter for Gila or Pima
-        elif county == "Gila" or county == "Pima":
-            early_votes_indices = [17,23]
-            polling_places_indices = [23,29]
-        #alter for Navajo
-        elif county == "Navajo":
-            provisional_indices = [35,41]
-        #alter for Pinal
-        elif county == "Pinal":
-            if year == "2016_pres":
-                provisional_indices = [35, 41]
-            precinct_name_indices = [207,235]
-        #alter for La Paz
-        elif county == "La_Paz":
-            early_votes_indices = [17,23]
-            polling_places_indices = [23,29]
-            precinct_name_indices = [207,235]
-        #alter for Yuma:
-        elif county == "Yuma":
-            provisional_indices = [35,41]
         #parse ElectionWare nonsense formatting
         data = file.readlines()
         #rows = []
