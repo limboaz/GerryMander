60d0ec8ff7b2c020a6628d4b8ca56954f4dd9f6c
Isaac Duarte <isaacpd@yahoo.com>
2020-03-01 14:46:04 -0500

    Separate material module imports

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
-import { MenuComponent } from './menu/menu.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MapComponent } from './map/map.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AttributeMenuComponent } from './attribute-menu/attribute-menu.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatExpansionModule} from '@angular/material/expansion';
+import {MenuComponent} from './menu/menu.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MapComponent} from './map/map.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AttributeMenuComponent} from './attribute-menu/attribute-menu.component';
+import {MaterialModules} from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -32,17 +25,10 @@ import {MatExpansionModule} from '@angular/material/expansion';
     BrowserAnimationsModule,
     LayoutModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatMenuModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatExpansionModule
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
